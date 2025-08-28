3dcff383c2d6ec6d070e0ba36afeba539593ef3e
AseelT <aseel.tah05@gmail.com>
2025-08-28 18:44:55 +0300

    add-profile-analytics

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -2,11 +2,13 @@ import Header from '../Components/Header.jsx';
 import MeetingsManager from '../Components/MeetingsManager.jsx';
 import Footer from '../Components/Footer.jsx';
 import ProfileMain from '../Components/ProfileMain.jsx'
+import MeetingHeapmap from '../Components/MeetingHeatmap.jsx';
 function Profile() {
   return(
     <>
       <Header showGradient = {false}/>
       <ProfileMain />
+      <MeetingHeapmap />
       <MeetingsManager />
       <Footer/>
     </>
